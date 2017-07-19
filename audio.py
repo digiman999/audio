40ee8cab9cb6df6713815f961bb14d1e0f9a7acd
Eddie Howell <eddiejhowell@gmail.com>
2017-07-19 10:40:47 +0100

    1

---
+++ audio.py
@@ -0,0 +1,37 @@
+
+
+class Audio:   # an Audio object (not a file system file)
+    pass
+
+
+
+
+'''
+Fingerprinting
+- uses
+    - phrases/sentences that trigger something ex applauding, anger
+    - voice recognition
+    - object recognition
+        - different voices: ex phone conversation
+    - noise/gap removal
+        - auto edit recordings ex phone conversations to quickly navigate to important parts
+    - security
+    - legal
+    - monitoring any streams
+    - intellectual copyright
+        - royalty collection
+        - enforce copyright
+    - forensics
+        -
+- how
+    - Industrial Strength AUdio Search Algorithm - Andrew Wang, 7 pages long, from Shazam
+    - analyse different dimensions
+        - pitch
+        - dynamics
+        - spectrum
+        - tempo/time span of all other dimensions
+
+'''
+
+from dejavu import Dejavu
+
